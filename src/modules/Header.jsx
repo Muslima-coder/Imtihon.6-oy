7e5c53913e3f190a92da7ed49572cadbce779cd6
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-07-15 23:36:48 +0500

    update commit

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -7,39 +7,54 @@ const Header = () => {
   const navList = ["Home", "About me", "Pages", "Contact us"]
 
   return (
-   <header className="py-[57px] relative">
-  <div className="containers flex justify-around sm:justify-between items-center">
-    <a href="#">
-      <img
-        className="sm:w-[176px] sm:h-[41px] w-[100px] h-[30px]"
-        src={Logo}
-        alt="logo"
-      />
-    </a>
+    <header className="py-[57px] relative">
+      <div className="containers flex justify-around sm:justify-between items-center">
+        <a href="#">
+          <img
+            className="sm:w-[176px] sm:h-[41px] w-[100px] h-[30px]"
+            src={Logo}
+            alt="logo"
+          />
+        </a>
 
-    <div className="relative z-[999]">
-      <nav className="hidden sm:flex gap-[40px] items-center">
-        {navList.map((item, index) => (
-          <NavLink key={index} title={item} />
-        ))}
-      </nav>
+        {/* Nav & Button */}
+        <div className="relative z-[20]">
+          <nav className="hidden sm:flex gap-[40px] items-center">
+            {navList.map((item, index) => (
+              <NavLink key={index} title={item} />
+            ))}
+          </nav>
 
-      <button className="sm:hidden flex" onClick={() => setIsOpen(!isOpen)}>
-        {/* icon */}
-      </button>
-    </div>
-  </div>
+          {/* Toggle Button */}
+          <button
+            className="sm:hidden flex"
+            onClick={() => setIsOpen(!isOpen)}
+            aria-expanded={isOpen}
+            aria-label="Toggle Menu"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              className="bi bi-three-dots"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3" />
+            </svg>
+          </button>
+        </div>
+      </div>
 
-  {/* menu endi container tashqarisida */}
-  {isOpen && (
-    <div className="absolute right-4 top-full bg-white shadow-md rounded-md flex flex-col gap-[20px] p-4 sm:hidden z-[9999] w-[200px]">
-      {navList.map((item, index) => (
-        <NavLink key={index} title={item} />
-      ))}
-    </div>
-  )}
-</header>
-
+      {/* Mobile Menu (outside of .containers) */}
+      {isOpen && (
+        <div className="absolute top-full right-4 bg-white shadow-md rounded-md flex flex-col gap-[20px] p-4 sm:hidden z-[10] w-[200px]">
+          {navList.map((item, index) => (
+            <NavLink key={index} title={item} />
+          ))}
+        </div>
+      )}
+    </header>
   )
 }
 
