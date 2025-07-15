e76d472c6afb1a72520aae49f4f54ac2d9f54c48
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-07-15 15:34:12 +0500

    update commit

--- src/modules/Hero.jsx
+++ src/modules/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
   return (
     <section className='hero-section'>
         <div className="containers">
-            <h2 className=' text-[25px] leading-0 mt-[15px] mb-[3px] sm:mt-[23px] text-center font-normal sm:text-[100px] text-[rgba(36,47,101,1)] sm:leading-[130px] '>Tell your creativity to the world with <span className='text-[rgba(255,119,76,1)]'> UIUXer</span></h2>
-            <p className='mb-[15px] w-[500px] mx-auto text-center font-normal text-[25px] text-[rgba(36,47,101,1)] leading-[37px]'>UIUXer is a creative, minimal, and cleanly designed personal and portfolio Webflow template.</p>
+            <h2 className=' text-[25px] leading-[35px] mt-[15px] mb-[3px] sm:mt-[23px] text-center font-normal sm:text-[100px] text-[rgba(36,47,101,1)] sm:leading-[130px] '>Tell your creativity to the world with <span className='text-[rgba(255,119,76,1)]'> UIUXer</span></h2>
+            <p className='mb-[15px] w-[300px] sm:w-[500px] mx-auto text-center font-normal sm:text-[25px] text-[rgba(36,47,101,1)] sm:leading-[37px] text-[14px] leading-[18px] '>UIUXer is a creative, minimal, and cleanly designed personal and portfolio Webflow template.</p>
             <div className='justify-center flex items-center gap-[10px] mb-[30px] sm:mb-[60px]'>
                 <Button title={"Buy Template"} extraClass={"bg-[#FF774C] border-[#FF774C] hover:text-[#FF774C]  "}/>
                 <Button title={"Buy Template"} extraClass={"bg-[#7390F9] border-[#7390F9] hover:text-[#7390F9]  "}/>
