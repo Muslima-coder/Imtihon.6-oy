6f1a0ea1af51c1f419a6e7a9613d8c1d85ff8bd2
Muslima-coder <muslimayoqubova2008@gmail.com>
2025-07-15 20:25:37 +0500

    update commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = ({srcImg, wImg, hImg, extraClass, text, extraClassText}) => {
   return (
     <div className="sm:w-[600px] w-[560px] ">
         <img className={`card-content rounded-[20px] mb-[39px] ${extraClass}`} src={srcImg} alt="photo" width={wImg} height={hImg}/>
-        <strong className={`mb-[80px] text-center flex justify-center font-normal text-[25px] leading-[37px] text-[#242F65] ${extraClassText}`}>{text}</strong>
+        <strong className={`duration-300 hover:text-[rgba(255,119,76,1)] mb-[80px] text-center flex justify-center font-normal text-[25px] leading-[37px] text-[#242F65] ${extraClassText}`}>{text}</strong>
     </div>
   )
 }
